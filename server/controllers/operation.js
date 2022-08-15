bc13fb46bd00b87e036393787710bc9a2594720b
maxiferrioli <maxiferrioli.97@outlook.es>
2022-08-15 09:56:05 -0300

    add methods

--- server/controllers/operation.js
+++ server/controllers/operation.js
@@ -1,3 +1,14 @@
 exports.getOperation = (req, res, next) => {
-  res.send("operations");
+  res.send("get operation");
+};
+exports.addOperation = (req, res, next) => {
+  res.send("Add");
+};
+
+exports.updateOperation = (req, res, next) => {
+  res.send("Update");
+};
+
+exports.deleteOperation = (req, res, next) => {
+  res.send("Delete");
 };
--- server/routes/operation.js
+++ server/routes/operation.js
@@ -3,5 +3,8 @@ const operationController = require("../controllers/operation");
 const router = express.Router();
 
 router.get("/list", operationController.getOperations);
+router.post("/", operationController.addOperation);
+router.patch("/", operationController.updateOperation);
+router.delete("/", operationController.deleteOperation);
 
 module.exports = router;
