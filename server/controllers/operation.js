cf7610a48bf5b31aa32f19aa737020bc0c546322
maxiferrioli <maxiferrioli.97@outlook.es>
2022-08-15 23:41:11 -0300

    get operations methods

--- server/controllers/operation.js
+++ server/controllers/operation.js
@@ -1,6 +1,19 @@
+const Operation = require("../models/operation");
+
 exports.getOperation = (req, res, next) => {
+  Operation.findAll({}).then(() => {
+    res.status(200).json({ operations, messege: "get operation" });
+  });
   res.send("get operation");
 };
+
+exports.getOperation = (req, res, next) => {
+  Operation.findAll({}).then(() => {
+    res.status(200).json({ operations, messege: "get operation" });
+  });
+  res.send("get operation");
+};
+
 exports.addOperation = (req, res, next) => {
   res.send("Add");
 };
