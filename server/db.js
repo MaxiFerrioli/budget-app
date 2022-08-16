4870748ad7ba22071313ffbb155a746000191d4c
maxiferrioli <maxiferrioli.97@outlook.es>
2022-08-15 23:22:15 -0300

    connects to db w/.env

---
+++ server/db.js
@@ -0,0 +1,13 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASS,
+  {
+    dialect: "mysql",
+    host: process.env.DATABASE_HOST,
+  }
+);
+
+module.exports = sequelize;
