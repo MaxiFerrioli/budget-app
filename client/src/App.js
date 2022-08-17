6f25c7acabb3cbec969bee9381d21a7b876efcb3
maxiferrioli <maxiferrioli.97@outlook.es>
2022-08-17 20:16:20 -0300

    fix error & add browserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
