e2f2bc1c7e5211c25ec832fd89d10cef1cdca0a0
maxiferrioli <maxiferrioli.97@outlook.es>
2022-08-16 23:47:19 -0300

    adds cors

--- server/app.js
+++ server/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const operationRoutes = require("./routes/operation");
+require("dotenv").config();
+var cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("APP CORRIENDO");
