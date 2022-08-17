23a271e7c409e975b835e638d839e0e2abbd447a
maxiferrioli <maxiferrioli.97@outlook.es>
2022-08-17 20:14:59 -0300

    install router dom, create Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
+import { Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div>IM HOME</div>;
+}
+
+export default Home;
