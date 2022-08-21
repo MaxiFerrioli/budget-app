29d2dd1fdbeb938430ac35cd1a6f871dcb253a99
maxiferrioli <maxiferrioli.97@outlook.es>
2022-08-21 00:00:40 -0300

    news navlinks

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,27 @@
 import { Toolbar } from "../Toolbar/Toolbar";
+import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ children }) => {
   return (
     <div>
-      <Toolbar>Budget</Toolbar>
+      <Toolbar>
+        <div>Budget app</div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink exact={true} to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/operations">Operations</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </Toolbar>
+      <div>{children}</div>
     </div>
   );
 };
 
 export default Navbar;
-   
