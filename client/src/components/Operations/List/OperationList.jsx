6bb295c678d64383d44950bbc8b88de7e7398db1
maxiferrioli <maxiferrioli.97@outlook.es>
2022-08-21 14:02:39 -0300

    implements showActions in Operations page

--- client/src/components/Operations/List/OperationList.jsx
+++ client/src/components/Operations/List/OperationList.jsx
@@ -9,7 +9,11 @@ const types = [
   { id: 2, name: "expense" },
 ];
 
-const OperationsList = ({ operations, onDeleteOperation }) => {
+const OperationsList = ({
+  operations,
+  onDeleteOperation,
+  showActions = false,
+}) => {
   const getTypeName = (operationType) => {
     const type = types.find((type) => type.id === operationType);
     return type.name;
@@ -28,14 +32,16 @@ const OperationsList = ({ operations, onDeleteOperation }) => {
               <div>{moment(operation.date).format("DD/MM/YYYY")}</div>
               <div>{getTypeName(operation.type_id)}</div>
             </div>
-            <div>
+            {showActions && (
               <div>
-                <Link to={`/operation/edit/${operation.id}`}>
-                  <FiEdit />
-                </Link>
-                <BsTrash onClick={() => onDeleteOperation(operation.id)} />
+                <div>
+                  <Link to={`/operation/edit/${operation.id}`}>
+                    <FiEdit />
+                  </Link>
+                  <BsTrash onClick={() => onDeleteOperation(operation.id)} />
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </Item>
       ))}
