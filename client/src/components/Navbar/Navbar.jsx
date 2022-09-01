44f154afedf1f09bcd5906988f4188e770d1869f
maxiferrioli <maxiferrioli.97@outlook.es>
2022-09-01 01:05:51 -0300

    delete toolbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,21 @@
-import { Toolbar } from "../Toolbar/Toolbar";
 import { NavLink } from "react-router-dom";
 
-const Navbar = ({ children }) => {
+const Navbar = () => {
   return (
     <div>
-      <Toolbar>
-        <div>Budget app</div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink exact={true} to="/">
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/operations">Operations</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </Toolbar>
-      <div>{children}</div>
+      <div>Budget app</div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink exact={true} to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/operations">Operations</NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
--- client/src/components/Toolbar/Toolbar.jsx
+++
@@ -1,4 +0,0 @@
-export const Toolbar = ({ children }) => {
-  return <div>{children}</div>;
-};
-export default Toolbar;
