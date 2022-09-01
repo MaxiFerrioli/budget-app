3fb875794f8aa2f7b9819e6ef0d73047d3b08461
maxiferrioli <maxiferrioli.97@outlook.es>
2022-09-01 02:38:10 -0300

    Navbar Styles

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,40 @@
 import { NavLink } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import "./Navbar.css";
 
-const Navbar = () => {
+const Navigate = () => {
   return (
     <div>
-      <div>Budget app</div>
-      <nav>
-        <ul>
-          <li>
-            <NavLink exact={true} to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/operations">Operations</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <Navbar bg="secondary" expand="md" className="mb-3">
+        <Container fluid>
+          <Navbar.Brand href="#">Budget app</Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-lg`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
+                Budget app
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end pl-5 flex-grow-1 pe-3">
+                <NavLink className="pe-3" exact={true} to="/">
+                  Home
+                </NavLink>{" "}
+                <NavLink to="/operations">Operations</NavLink>{" "}
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
     </div>
   );
 };
 
-export default Navbar;
+export default Navigate;
